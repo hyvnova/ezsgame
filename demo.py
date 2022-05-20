ccc8dec408dc62422e005c42785ab9ce7174ac9b
Hendrick <70229868+EZSNoVa@users.noreply.github.com>
2022-05-20 15:17:42 -0400

    v2.1.5.4 Hot Reload (Experimental)

--- demo.py
+++ demo.py
@@ -2,16 +2,14 @@ from ezsgame.all import *
 
 screen = Screen(title="Sample Demo")
 
-obj = Rect(pos=[0,0], size=[50,50], screen=screen, components=[Draggable])
+obj = Rect(pos=[0,0], size=[50,50], components=[Draggable])
 
-gradient = Gradient(screen, "blue", "pink", complexity=70)
+gradient = Gradient("blue", "pink", complexity=70)
 
 while True:
     screen.check_events()
     screen.fill(gradient)
     
-    
     obj.draw()
     
-
     screen.update()
