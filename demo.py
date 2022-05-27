20548292c4f908fe2d49e9501b59e40c3724785e
Rodriguez Vargas <hyr001@bcps.org>
2022-05-27 09:56:35 -0400

    Align random test cases

--- demo.py
+++ demo.py
@@ -1,12 +1,16 @@
 from ezsgame.all import *
+import random
 
 screen = Screen(title="Sample Demo")
 
 def gen_objects(n, size=[50,50]):
-    return [Rect([0,0], size, color=random_color()) for _ in range(n)]
+    return [Rect([0,0], 
+                [ random.randint(size[0]/2, size[0]*1.5), 
+                random.randint(size[1]/2, size[1]*1.5)],
+                color=random_color()) for i in range(n)] 
 
 plane = Interface(screen)
-plane.add(gen_objects(2, [100,50])).align("row")
+plane.add(gen_objects(9, [100,100])).align("row")
 
 while True:    
     screen.check_events()
