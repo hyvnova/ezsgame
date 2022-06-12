583a2e0b9f85b3e929fc05e6707d18c5543ba0d4
NoVa <thenovaggamner@gmail.com>
2022-06-12 09:13:21 -0400

    current

--- demo.py
+++ demo.py
@@ -1,16 +1,13 @@
-from ezsgame.all import *
+from ezsgame.basics import *
 
-screen = Screen(title="Sample Demo", fps=120, show_fps=True)
+screen = Screen(title="Sample Demo", fps=144, show_fps=True)
 
 @screen.on_key("down", ["escape"])
 def quit_app():
 	screen.quit()
 
 
-player = Rect(["center", "center"], [50,50], color="white", 
-	components=[
-		Controllable, Draggable
-	])
+player = Rect(["center", "center"], [50,50], color="white")
 
 while True:    
     
