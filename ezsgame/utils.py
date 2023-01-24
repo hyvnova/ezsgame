27d74ecc2662abfc96d8fa77dd14d7b1161a88ba
NoVa <thenovaggamer@gmail.com>
2023-01-24 06:39:12 -0500

    3.2 Added utils methods and Fixed controller

--- ezsgame/utils.py
+++ ezsgame/utils.py
@@ -1,5 +1,7 @@
 
-from typing import overload
+import math
+from typing import Tuple, overload
+from .types import Vector2
 
 
 def clamp(value, minimum, maximum):
@@ -44,3 +46,33 @@ def map_range(value: float, start1: float, stop1: float, start2: float, stop2: f
     mapped value
     """
     return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2
+
+
+def normalize(vector: Tuple[float, float] | Vector2) -> Vector2:
+    """
+    Normalize a 2D vector
+    
+    ### Params
+    - `vector`: vector to be normalized
+    
+    ### Return
+    Normalized vector
+    """
+    x, y = vector
+    length = math.sqrt(x**2 + y**2)
+    return x/length, y/length
+
+def distance(p1: Tuple[float, float] | Vector2, p2: Tuple[float, float] | Vector2) -> float:
+    """
+    Calculates distance between two points
+    
+    ### Params
+    - `p1`: first point
+    - `p2`: second point
+    
+    ### Return
+    distance
+    """
+    x1, y1 = p1
+    x2, y2 = p2
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
