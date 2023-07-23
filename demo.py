a7376eaf0122db30ae8532f92155470cad30fd93
NoVa <thenovaggamner@gmail.com>
2023-07-23 14:50:52 -0400

    v0.5.1

--- demo.py
+++ demo.py
@@ -1,18 +1,26 @@
 from ezsgame import *
 
-window = Window(Size(800, 600), title="Just a square", profiling=ProfilingOptions())
+window = Window(Size(800, 600), title="Just a square")
 
 square = Rect(
     Pos("center"),
     Size(100, 100),
 )
 
+timer = Text("0", Pos("center", "center"), font_size=26, color="white")
+
 controller = Controller()
 
 
+# update timer
+@add_interval(1000, repeat=3)
+def update_timer():
+    timer.text.set(str(int(timer.text.get()) + 1))
+
+
 # quit after 10 seconds (used for profiling)
-@add_interval(Milliseconds.from_unit(TimeUnit.seconds, 10), "quit")
-def quit():
+@add_interval(10 * 1000)
+def quit_window():
     window.quit()
 
 
@@ -22,6 +30,7 @@ while True:
 
     square.pos += controller.get_speed("simple")
 
-    square.draw()
+    # square.draw()
+    timer.draw()
 
     window.update()
